0665c625aeb652f361cd91fdb8218d78300c2b17
Raphael <ralpha19@hotmail.com>
2018-10-31 00:47:07 +0000

    added funciton to generate short URL and insert key value pair of URL into urlDatabase

---
+++ express_server.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var PORT = 8080; // default port 8080
+
+//This tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+// the path /urls/new actually matches the /urls/:id pattern (with the :id placeholder matching the string "new" instead of an actual id. Another way to say it is that in case of overlap, routes should be ordered from most specific to least specific.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get('/_headers', function (req, res) {
+  res.send('GET request to the homepage')
+})
+
+
+app.post("/urls", (req, res) => {
+
+
+  console.log(req.body);  // debug statement to see POST parameters
+
+  console.log(urlDatabase[generateRandomString] = req.body.longURL);
+  // TODO: Update the valurues in urlDatabase
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let randomString = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i <= 5; i++) {
+    randomString += possible[(Math.floor(Math.random() * possible.length))];
+  }
+  return randomString;
+}
+
+
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  res.redirect(longURL);
+});
+
+
+
+
+
+
+
+
+
+
+
+
